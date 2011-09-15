807831e6e02b8f0c876ff37051034df82c7e959e
David Winter <winda002@student.otago.ac.nz>
2011-09-15 21:09:46 +1200

    now with functional programming (death to for loops :)

--- R/MMD.stable.R
+++ R/MMD.stable.R
@@ -1,11 +1,17 @@
-stable.dist <- function(alignment){
+#
+#Calculate the expected distribution of mismatches for an alignment under teh 
+#assumption of stable population size
+#
+
+MMD.stable <- function(alignment){
   mismatch <- dist.dna(alignment, "N")
   theta = mean(mismatch)
   upper = ceiling(max(mismatch))
-  mm <- 0:upper
-  count <- numeric()
-  for (i in 0:upper){
-    count[i] <- (theta^i) / ((theta + 1)^(i+1) )
-  }
+  count <- sapply(0:upper, function(i) (theta^i) / ((theta + 1)^(i+1)))
   return(count)
 }
+
+#> MMD(woodmouse, legend=FALSE)
+#> lines(MMD.stable(woodmouse), col='red')
+#> legend('topleft', c("Emperical", "Stable expectation"), col=c("blue", "red"), bg="white", lty=1)
+
