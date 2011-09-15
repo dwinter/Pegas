f8272177de279c41e339e0e566c484a896af7134
David Winter <winda002@student.otago.ac.nz>
2011-09-15 20:55:42 +1200

    diversity.indices returns a one row DF because lists print funny

--- R/diversity.indices.R
+++ R/diversity.indices.R
@@ -6,16 +6,26 @@
 
 diversity.indices <- function(d){
   haplo <- haplotype(d)
-  result <- c(
+  result <- data.frame(
     "n" = dim(d)[1], 
     "n haplotypes" = dim(haplo)[1],
     "h" = h.div(haplo),
     "seggregating sites" = length(seg.sites(d)),
     "nucleotide diversity" =  nuc.div(d)
   )
-  #class(result) <- "diversity.index"
+  row.names(result) <- as.character(bquote(d))
   return(result)
 }
 
-
+#
+#data(woodmouse)
+#> diversity.indices(woodmouse)
+#   n n.haplotypes h seggregating.sites nucleotide.diversity
+#d 15           15 1                 56           0.01294610
+#set.seed(123)
+#> to.sample <- sample(1:dim(woodmouse)[1], 20, replace=TRUE)
+#> d <- woodmouse[to.sample, ]
+#> diversity.indices(d)
+#   n n.haplotypes         h seggregating.sites nucleotide.diversity
+#d 20           11 0.9368421                 55           0.01372922
 
