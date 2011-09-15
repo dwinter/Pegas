76ae5c0dc80cec62f842ce5f9775f99d9ee8120e
David Winter <winda002@student.otago.ac.nz>
2011-09-15 21:16:01 +1200

    added compare.diversity.R to calc. summary stats from populations

---
+++ R/compare.diversity.R
@@ -0,0 +1,20 @@
+#
+#Calculate summary statistics for one or more populations from a single
+#alignment
+#
+
+compare.diversity <- function(d, sites){
+  #TODO 
+  #catch error re passing a haplotype (not DNAbin)
+  n <-table(sites)
+  names(sites) <- 1:dim(d)[1]
+  h <- haplotype(d)
+  u.sites <- unique(sites)
+  h.index <- sapply(attr(h, 'index'), function(s) unique(sites[s]))
+  private <- sapply(u.sites, function(s) sum(h.index==s))
+  n.haplo <- sapply(u.sites, function(s) sum(sapply(h.index, function(h) s %in% h)))
+  pi <- sapply(u.sites, function(s) nuc.div(d[sites==s,]))
+  segregating.sites <- sapply(unique(sites), function(s) length(seg.sites(d[sites==s,])))
+  h.diversity <- sapply(sapply(u.sites, function(s) haplotype(d[sites==s,])), h.div)
+  return(cbind(n, n.haplo,private,h.diversity,pi, segregating.sites))
+}
